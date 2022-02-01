97643da1646ea17ff296e4bc3fb3455ca7fa2a05
unknown <parodirodrigo0@gmail.com>
2022-01-31 22:24:57 -0300

    ejercicio terminado

--- 1-EntradaSalida/jsEntradaSalida-03.js
+++ 1-EntradaSalida/jsEntradaSalida-03.js
@@ -1,11 +1,14 @@
 /*
+Parodi Rodrigo
+e/s 03
 Debemos lograr tomar un dato por 'ID'
 y luego mostrarlo por 'Alert' al presionar el botón  'mostrar'*/
 function mostrar()
 {
 	var nombreIngresado;
-	nombreIngresado=txtIdNombre.value;
-	alert("ok");
+	// esta mal nombreIngresado=txtIdNombre.value;
+	nombreingresado= document.getElementById("txtIdNombre").value;
+	alert("su nombre es "+ nombreingresado);
 
 }
 
--- 1-EntradaSalida/jsEntradaSalida-04.js
+++ 1-EntradaSalida/jsEntradaSalida-04.js
@@ -1,9 +1,14 @@
 /* 
+Rodrigo Parodi
+e/s 04
 Debemos lograr tomar un dato por 'PROMPT' 
 y lo muestro por 'ID' al presionar el botón 'mostrar'
 */
 function mostrar()
 {
-	alert("ok");
+	let nombreingresado;
+	nombreingresado = prompt("ingrese su nombre", "Rodrigo");
+	document.getElementById('txtIdNombre').value= "su nombre es "+ nombreingresado;
+
 }
 
