58a29b1f553da2b42baae063237ec3e4066fe2c2
unknown <parodirodrigo0@gmail.com>
2022-02-01 22:33:44 -0300

    ejercicios realizados

--- 1-EntradaSalida/jsEntradaSalida-09.js
+++ 1-EntradaSalida/jsEntradaSalida-09.js
@@ -7,23 +7,38 @@ mostrar el importe con un aumento del 10 %
 en el cuadro de texto "RESULTADO".*/
 function mostrarAumento()
 {
+	//let sueldo;
+	//let resultado;
+	//let resultadoSumado;
+
+	//sueldo=parseInt(document.getElementById('txtIdSueldo').value);
+	
+	//resultado=sueldo*10/100;
+
+	//resultadoSumado=sueldo + resultado;
+
+	//document.getElementById('txtIdResultado').value=resultadoSumado;
+
+
+//EJERCICIO 9 BIS
+//en el ejercicio 9 e/s
+//lo anterior mas
+//se debe pedir el % por prompt
+
 	let sueldo;
 	let resultado;
 	let resultadoSumado;
+	let porcentaje;
 
 	sueldo=parseInt(document.getElementById('txtIdSueldo').value);
-	
-	resultado=sueldo*10/100;
+
+	porcentaje=prompt("ingrese su porcentaje ");
+
+	resultado=sueldo*porcentaje/100;
 
 	resultadoSumado=sueldo + resultado;
 
 	document.getElementById('txtIdResultado').value=resultadoSumado;
-
-
-	
-
-
-	
 }
 
 //'txtIdSueldo'
--- 1-EntradaSalida/jsEntradaSalida-10.js
+++ 1-EntradaSalida/jsEntradaSalida-10.js
@@ -7,19 +7,38 @@ mostrar el importe con un Descuento del 25 %
 en el cuadro de texto "RESULTADO"*/
 function mostrarAumento()
 {
+	//let importe;
+	//let importeFinal;
+	//let descuento;
+
+	//importe=parseInt(document.getElementById('txtIdImporte').value);
+
+	//importeFinal=importe*25/100;
+
+	//descuento=importe - importeFinal;
+
+	//document.getElementById('txtIdResultado').value = descuento;
+
+//EJERCICIO 10 BIS
+//RODRIGO PARODI
+//en el ejercicio 10 e/s
+//lo anterior mas
+//se debe pedir el % por prompt
+
 	let importe;
 	let importeFinal;
 	let descuento;
+	let porcentaje;
 
 	importe=parseInt(document.getElementById('txtIdImporte').value);
 
-	importeFinal=importe*25/100;
+	porcentaje=prompt("ingrese su porcentaje ");
+
+	importeFinal=importe*porcentaje/100;
 
 	descuento=importe - importeFinal;
 
 	document.getElementById('txtIdResultado').value = descuento;
-
-	
 }
 
 //'txtIdImporte'
