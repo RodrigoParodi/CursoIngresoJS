278d20d941fc439b35cab22564a451dcdbd9271c
unknown <parodirodrigo0@gmail.com>
2022-02-15 02:03:50 -0300

    terminados

--- 3-InstruccionSwitch/jsInstruccionSwitch-10.js
+++ 3-InstruccionSwitch/jsInstruccionSwitch-10.js
@@ -1,6 +1,55 @@
+//Instruccion Switch
+//Parodi Rodirog
+//Ejercicio 10
 function mostrar()
 {
 	var estacionIngresada =txtIdEstacion.value;
-	alert(estacionIngresada);
+	let destino;
+	let mensaje;
+	
+	estacionIngresada=document.getElementById('txtIdEstacion').value;
+	destino=document.getElementById('txtIdDestino').value;
 
-}//FIN DE LA FUNCIÓN
+	switch(estacionIngresada)
+	{
+		case "Invierno":
+			switch(destino)
+			{
+				case "Bariloche":
+					mensaje="Se puede viajar a "+destino;
+				break;
+				default:
+					mensaje="No se puede viajar a "+destino;
+				break;
+			}
+		break;
+		case "Verano":
+			switch(destino)
+			{
+				case "Mar del plata":
+				case "Cataratas":
+					mensaje="Se puede viajar a "+destino;
+				break;
+				default:
+					mensaje="No se puede viajar a "+destino;
+				break;
+			}
+		break;
+		case "Primavera":
+			switch(destino)
+			{
+				case "Bariloche":
+					mensaje= "No se puede viajar a "+destino;
+				break;
+				default:
+					mensaje="Se puede viajar a "+destino;
+				break;
+			}
+		break;
+		default:
+			mensaje="Se puede viajar a "+destino;
+		break;
+	}
+
+	alert(mensaje);
+}
